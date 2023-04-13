2c92a0bb7e33fd49a1c50978c4fc7815dfa4b7b3
Mmmad Amin Mahjub Asl <aminmahjoob3172@gmail.com>
2023-04-13 08:28:23 +0330

    Refactor the project

--- app.js
+++ app.js
@@ -1,106 +1,110 @@
 class Question {
-    constructor(title, choices, correctChoice) {
-        this.title = title;
-        this.choices = choices;
-        this.correctChoice = correctChoice;
-        this.yourAnswer = null;
+    constructor(question, options, correctAnswer) {
+        this.question = question;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+        this.userAnswer = null;
         this.isAnswered = false;
     }
 }
 
 class Quiz {
     constructor() {
-        this.quizes = [
+        this.questions = [
             new Question('What do you understand by HTML?', ['HTML describes the structure of a webpage', 'HTML is the standard markup language mainly used to create web pages', 'HTML consists of a set of elements that helps the browser how to view the content', 'All of the above', ], 3),
             new Question('Who is the father of HTML?', ['Rasmus Lerdorf', 'Tim Berners-Lee', 'Brendan Eich', 'Sergey Brin'], 1),
             new Question('HTML stands for ___', ['HyperText Markup Language', 'HighText Markup Language', 'HyperText Marking Language', 'High Marking Language'], 0),
             new Question('Which tag is used for inserting the largest heading in HTML?', ['head', '<h1>', '<h6>', 'heading'], 1),
-            new Question('Which is used to read an HTML page and render it?', ['Web server', 'Web network', 'Web browser', 'Web matrix'], 0)
+            new Question('Which is used to read an HTML page and render it?', ['Web server', 'Web network', 'Web browser', 'Web matrix'], 1)
         ];
         this.score = 0;
-        this.currentQuestionNum = 0;
+        this.currentQuestionIndex = 0;
     }
 
     calculateScore() {
-        const everyQuizScore = 100 / this.quizes.length;
+        const everyQuizScore = 100 / this.questions.length;
         this.score += everyQuizScore;
     }
 }
 
 class UpdateView {
-    constructor(quizes) {
-        this.quizesArr = quizes;
-        this.counterBtns = document.querySelectorAll('.quesiton-counter button');
-        this.choicesRoot = document.querySelector('.choices');
+    constructor(quiz) {
+        this.quiz = quiz;
+        this.nextPreviousBtns = document.querySelectorAll('.question-next-previuos button');
+        this.OptionsContainer = document.querySelector('.options');
         
-        this.counterBtns.forEach(counterBtn => counterBtn.addEventListener('click', () => {
-            counterBtn.classList[0] === 'previous-btn' ? this.nextPreviousActions(--this.quizesArr.currentQuestionNum) : this.nextPreviousActions(++this.quizesArr.currentQuestionNum);
-        }));
+        this.nextPreviousBtns.forEach(nextPreviousBtn => nextPreviousBtn.addEventListener('click', () => nextPreviousBtn.classList[0] === 'previous-btn' ? 
+            this.nextPreviousActions(--this.quiz.currentQuestionIndex) : 
+            this.nextPreviousActions(++this.quiz.currentQuestionIndex)
+        ));
     }
 
-    updateUI(currentQuestionNum) {
+    renderView(currentQuestionIndex) {
+        let questions = this.quiz.questions;
+        console.log(quiz);
+        const { question, options, correctAnswer } = questions[currentQuestionIndex];
+
         const questionTitle = document.querySelector('.question p'),
             currentNumberQuestion = document.querySelector('.current-number-question'),
-            currentQuestion = this.quizesArr.quizes[currentQuestionNum],
-            score = document.querySelector('.score span');
+            showScore = document.querySelector('.score span');
 
-        let { title, choices, correctChoice, yourAnswer, isAnswered } = currentQuestion;
             
-        questionTitle.textContent = title;
-            
-        choices.forEach((choice, index) => {
-            const choiceBtn = document.createElement('button');
-            
-            choiceBtn.classList.add('choice-btn');
-            choiceBtn.id = `${index}`;
-            choiceBtn.setAttribute('type', 'button');
-            choiceBtn.textContent = choice;
-            choiceBtn.addEventListener('click', e => {
-                e.stopPropagation();
-                currentQuestion.yourAnswer = +e.target.id;
-                currentQuestion.yourAnswer === correctChoice ? this.quizesArr.calculateScore() : null;
-                score.textContent = this.quizesArr.score;
+        questionTitle.textContent = question;
 
-                this.answeredAction({ correctChoice, yourAnswer: currentQuestion.yourAnswer });
-                currentQuestion.isAnswered = true;
+        options.forEach((option, i) => {
+            const optionBtn = document.createElement('button');
+            
+            optionBtn.classList.add('option-btn');
+            optionBtn.id = `${i}`;
+            optionBtn.setAttribute('type', 'button');
+            optionBtn.textContent = option;
 
-                console.log(currentQuestion);
+            optionBtn.addEventListener('click', e => {
+                e.preventDefault();
+debugger;
+                questions[currentQuestionIndex].userAnswer = +e.target.id;
+                questions[currentQuestionIndex].userAnswer === correctAnswer ? this.quiz.calculateScore() : null;
+                showScore.textContent = this.quiz.score;
+
+                this.answeredAction({ correctAnswer, userAnswer: questions[currentQuestionIndex].userAnswer });
+                questions[currentQuestionIndex].isAnswered = true;
             });
             
-            this.choicesRoot.append(choiceBtn);
+            this.OptionsContainer.append(optionBtn);
         });
         
-        currentNumberQuestion.textContent = `${currentQuestionNum + 1} / ${this.quizesArr.quizes.length}`;
-
-        // next privous acitions        
-        if (this.quizesArr.currentQuestionNum === 0) {
-            this.counterBtns[0].disabled = true;
-        } else if (this.quizesArr.currentQuestionNum === 4) {
-            this.counterBtns[1].disabled = true;
+        currentNumberQuestion.textContent = `${currentQuestionIndex + 1} / ${questions.length}`;
+        
+        if (currentQuestionIndex === 0) {
+            this.nextPreviousBtns[0].disabled = true;
+        } else if (currentQuestionIndex === 4) {
+            this.nextPreviousBtns[1].disabled = true;
         } else {
-            this.counterBtns[0].disabled = false;
-            this.counterBtns[1].disabled = false;
+            this.nextPreviousBtns[0].disabled = false;
+            this.nextPreviousBtns[1].disabled = false;
         }
     }
 
     answeredAction(currentQuestion) {
-        let { correctChoice, yourAnswer } = currentQuestion;
-        const choiceBtnsArr = document.querySelectorAll('.choices button');    
+        let { correctAnswer, userAnswer } = currentQuestion;
+        const optionBtns = document.querySelectorAll('.options button');    
     
-        choiceBtnsArr[correctChoice].classList.add('correct-answer');
-        this.choicesRoot.classList.add('disabled-btns');
-        yourAnswer !== correctChoice ? choiceBtnsArr[yourAnswer].classList.add('wrong-answer') : null;
+        optionBtns[correctAnswer].classList.add('correct-answer');
+        this.OptionsContainer.classList.add('disabled-btns');
+        console.log(optionBtns, userAnswer);
+        userAnswer !== correctAnswer ? optionBtns[userAnswer].classList.add('wrong-answer') : null;
     }
 
-    nextPreviousActions(currentQuestionState) {
-        let { isAnswered } = this.quizesArr.quizes[currentQuestionState]; 
-        this.choicesRoot.innerHTML = '';
-        this.choicesRoot.classList.remove('disabled-btns'); 
-        this.updateUI(currentQuestionState);
-        isAnswered ? this.answeredAction(this.quizesArr.quizes[currentQuestionState]) : null;
+    nextPreviousActions(currentQuestionIndex) {
+        const isAnswered  = this.quiz.questions[currentQuestionIndex].isAnswered; 
+
+        this.OptionsContainer.innerHTML = '';
+        this.OptionsContainer.classList.remove('disabled-btns'); 
+        this.renderView(currentQuestionIndex);
+        isAnswered ? this.answeredAction(this.quiz.questions[currentQuestionIndex]) : null;
     }
 }
 
 const quiz = new Quiz();
 const app = new UpdateView(quiz);
-app.updateUI(quiz.currentQuestionNum);
+app.renderView(quiz.currentQuestionIndex);
